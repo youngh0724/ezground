6de27d0e070325f7f113948991738393e87348f7
juncheolAn <ajcstar@naver.com>
2018-04-11 15:06:24 +0900

    안준철
    매치 컨트롤러 추가

--- ezground/src/main/java/com/ezground/teamproject/match/MatchController.java
+++ ezground/src/main/java/com/ezground/teamproject/match/MatchController.java
@@ -66,12 +66,6 @@ public class MatchController {
 	public String creatMatch(HttpSession session, MatchNotice matchNotice, RedirectAttributes redirectAttributes) {
 		logger.debug("creatMatch() matchNotice = {}", matchNotice.getMemberNo());
 
-		// 세션검사 로그인되어있지 않으면 홈화면으로
-		if (session.getAttribute("MemberLogin") == null) {
-			logger.debug("logout() 세션값 없으면 홈으로 리다이렉트 ");
-			return "redirect:/";
-		}
-
 		// 세션에서 맴버 번호를 받아온다.
 		MemberLogin memberLogin = (MemberLogin) session.getAttribute("MemberLogin");
 		int memberNo = memberLogin.getMemberNo();
@@ -125,6 +119,7 @@ public class MatchController {
 				
 		Map<String, Object> map;
 		
+		//홈팀 과 나의 팀을 비교하여 같으면 홈팀이화면에 필요한 정보 다르면 원정팀에 필요한 정보를 세팅하는 메서드를 호출한다.
 		if(homeTeamNo == myTeamNo) {
 			map = matchService.matchNoticeInfomationHome(matchNotice, memberNo);
 		} else {
@@ -309,12 +304,6 @@ public class MatchController {
 		logger.debug("matchJoinMemberInsert() matchNoticeNo = {}", matchNoticeNo);
 		logger.debug("matchJoinMemberInsert() homeAway = {}", homeAway);
 
-		// 세션검사 로그인되어있지 않으면 홈화면으로
-		if (session.getAttribute("MemberLogin") == null) {
-			logger.debug("logout() 세션값 없으면 홈으로 리다이렉트 ");
-			return "redirect:/";
-		}
-
 		// 세션으로부터 자신의 맴버번호 확인
 		MemberLogin memberLogin = (MemberLogin) session.getAttribute("MemberLogin");
 		int memberNo = memberLogin.getMemberNo();
@@ -407,21 +396,51 @@ public class MatchController {
 	}
 	
 	// 매치 공고 정보 조회화면에서 원정팀 신청 맴버가 조건달성되어 홈팀에 매치 요청을 하는 요청 처리
-	@RequestMapping(value = "/match/matchJoinTeam", method = RequestMethod.GET)
-	public String matchNoticeJoinTeam(HttpSession session, Model model,
+	@RequestMapping(value = "/match/matchRequestHomeTeam", method = RequestMethod.GET)
+	public String matchRequestHomeTeam(HttpSession session, Model model, RedirectAttributes redirectAttributes,
 			@RequestParam(value = "matchNoticeNo", required = true) int matchNoticeNo) {
-		logger.debug("matchNoticeDelete() matchNoticeNo = {}", matchNoticeNo);
+		logger.debug("matchRequestHomeTeam() matchNoticeNo = {}", matchNoticeNo);
 
 		// 세션검사 로그인되어있지 않으면 홈화면으로
 		if (session.getAttribute("MemberLogin") == null) {
-			logger.debug("logout() 세션값 없으면 홈으로 리다이렉트 ");
+			logger.debug("matchRequestHomeTeam() 세션값 없으면 홈으로 리다이렉트 ");
 			return "redirect:/";
 		}
+		
+		// 세션으로부터 맴버넘버를 받아온다.
+		MemberLogin memberLogin = (MemberLogin) session.getAttribute("MemberLogin");
+		logger.debug("matchRequestHomeTeam() memberNo = {}", memberLogin.getMemberNo());
+
+		// 현재 세션이 갖고있는 스포츠 종목
+		SportEntries sportEntries = (SportEntries) session.getAttribute("currentSportEntry");
+		int sportEntriesNo = sportEntries.getSportEntriesNo();
+		logger.debug("matchRequestHomeTeam() entryNo = {}", sportEntriesNo);
+
+		// 종목번호와 맴버번호로 팀번호를 조회한다.
+		Integer teamNo = matchService.teamNoSelectOne(sportEntriesNo, memberLogin.getMemberNo());
+		logger.debug("matchRequestHomeTeam() teamNo = {}", teamNo);
 
 		// 매치공고 다른팀이 검색할수 있도록 공개하기
-		matchService.matchNoticeNotice(matchNoticeNo);
+		matchService.matchRequestHomeTeam(matchNoticeNo, teamNo);
 
-		return "redirect:/match/matchSelectTeam";
+		// 리다이렉트 시킬시 정보 저장
+		redirectAttributes.addAttribute("matchNoticeNo", matchNoticeNo);
+		return "redirect:/match/matchNoticeInfomation";
+	}
+	
+	//홈팀의 원정팀 매치요청을 수락하는 요청 처리
+	@RequestMapping(value = "/match/matchRequestPermit", method = RequestMethod.GET)
+	public String matchRequestPermit(HttpSession session, Model model, RedirectAttributes redirectAttributes,
+			@RequestParam(value = "matchNoticeNo", required = true) int matchNoticeNo,
+			@RequestParam(value = "memberNo", required = true) int memberNo) {
+		logger.debug("matchRequestPermit() matchNoticeNo = {}", matchNoticeNo);
+		logger.debug("matchRequestPermit() memberNo = {}", memberNo);
+		
+		matchService.matchRequestPermit(matchNoticeNo, memberNo);
+		
+		// 리다이렉트 시킬시 정보 저장
+		redirectAttributes.addAttribute("matchNoticeNo", matchNoticeNo);
+		return "redirect:/match/matchNoticeInfomation";
 	}
 
 }
